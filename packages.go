c01c20222fd40b35d6d45cc1a1f77e8b0f946fd2
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-06-19 12:13:08 +0100

    initial commit

---
+++ packages.go
@@ -0,0 +1,9 @@
+/*
+
+errors: Provides functions to create and handle errors.
+fmt: Provides functions for formatted I/O (e.g., printing to the console).
+rand: Provides functions to generate random numbers.
+os: Provides functions to interact with the operating system (e.g., exiting the program).
+time: Provides functions for manipulating time and duration.
+
+*/
