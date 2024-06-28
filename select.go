1c83f692bd519c7cdd1664261071f9816ca8e348
TheInvincible <adesegunraphaeladeoye@gmail.com>
2024-06-28 07:50:29 +0100

    initial commit

--- select.go
+++ select.go
@@ -6,25 +6,32 @@ import (
 	"time"
 )
 
-func main() {
-	ctx := context.Background()
-
-	ctx = context.WithValue(ctx, "myKey", "myValue")
-
-	doSomething(ctx)
-}
 func doSomething(ctx context.Context) {
-	ctx, cancelCtx := context.WithCancel(ctx)
+	// Set a deadline for the context 1500 milliseconds from now
+	deadline := time.Now().Add(1500 * time.Millisecond)
+	ctx, cancelCtx := context.WithDeadline(ctx, deadline)
+	defer cancelCtx()
 
 	printCh := make(chan int)
+	// Start a goroutine to process numbers from the channel
 	go doAnother(ctx, printCh)
 
+	// Loop to send numbers 1, 2, and 3 into the printCh channel
 	for num := 1; num <= 3; num++ {
-		printCh <- num
+		select {
+		case printCh <- num:
+			// Successfully sent num to printCh, now sleep for 1 second
+			time.Sleep(1 * time.Second)
+		case <-ctx.Done():
+			// Context's deadline or cancellation is reached, exit the loop
+			break
+		}
 	}
 
+	// Cancel the context to clean up resources
 	cancelCtx()
 
+	// Wait for a short moment to allow the doAnother goroutine to finish
 	time.Sleep(100 * time.Millisecond)
 
 	fmt.Printf("doSomething: finished\n")
@@ -43,4 +50,10 @@ func doAnother(ctx context.Context, printCh <-chan int) {
 			fmt.Printf("doAnother: %d\n", num)
 		}
 	}
+}
+
+func main() {
+	ctx := context.Background()
+	doSomething(ctx)
+	fmt.Println("main: finished")
 }
